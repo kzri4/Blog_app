618c8392f074474979d1f6da712c187b1c479a0d
kzri4 <kazu4126ab@gmail.com>
2021-02-21 05:07:22 +0900

    second commit

--- server/public/users/log_in.php
+++ server/public/users/log_in.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css">
 
 <?php include __DIR__ . "/../common/_head.php"; ?>
 
--- server/public/users/new.php
+++ server/public/users/new.php
@@ -2,11 +2,7 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css">
+    <?php include __DIR__ . "/../common/_head.php"; ?>
     <title>ブログアプリ</title>
 </head>
 
